e123d38220d70cea855be6ab79d76a1b34f08312
GeniusCorn <hi@nicecorn.com>
2024-04-04 18:39:40 +0800

    feat: add ProseA component

---
+++ components/content/ProseA.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+defineProps({
+  href: {
+    type: String,
+    default: '',
+  },
+  target: {
+    // eslint-disable-next-line ts/ban-types
+    type: String as PropType<'_blank' | '_parent' | '_self' | '_top' | (string & {}) | null | undefined>,
+    default: undefined,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <NuxtLink
+    decoration-gray hover:decoration-momo transition-all ease-in-out duration-150
+    text-dark dark:text-white
+    :href="href"
+    :target="target"
+  >
+    <slot />
+  </NuxtLink>
+</template>
