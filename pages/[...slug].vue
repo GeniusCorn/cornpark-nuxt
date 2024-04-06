c6e44930392f083d455ddefb9ec4bca172e67e29
GeniusCorn <hi@nicecorn.com>
2024-04-06 19:23:34 +0800

    feat([...slug].vue): add dynamic content rendering and templates for default, empty, and not-found cases

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,32 @@
+<script lang="ts" setup>
+
+</script>
+
 <template>
-  <main>
-    <ContentDoc />
+  <main px-12 pb-12>
+    <ContentDoc>
+      <template #default="{ doc }">
+        <article prose m-auto font-sans>
+          <!-- <h1>{{ doc.title }}</h1> -->
+          <ContentRenderer :value="doc" />
+        </article>
+      </template>
+
+      <template #empty>
+        <article prose m-auto font-sans>
+          <h1>Document is empty.</h1>
+        </article>
+      </template>
+
+      <template #not-found>
+        <article prose m-auto font-sans>
+          <h1>Document not found.</h1>
+        </article>
+      </template>
+    </ContentDoc>
   </main>
 </template>
+
+<style scoped>
+
+</style>
