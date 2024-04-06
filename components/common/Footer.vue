1025d50ddb308f7a1416efbf0993c724b7b921c4
GeniusCorn <hi@nicecorn.com>
2024-04-06 21:35:15 +0800

    * feat(app.vue): add CommonFooter component to app.vue template

---
+++ components/common/Footer.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const currentYear = computed(() => new Date().getFullYear())
+</script>
+
+<template>
+  <div
+    flex flex-col items-center justify-center gap-2
+    text-xs opacity-60
+  >
+    <div>
+      Copyright &copy; 2021 - {{ currentYear }}
+      <NuxtLink
+        to="https://github.com/GeniusCorn" target="_blank"
+      >
+        GeniusCorn
+      </NuxtLink>
+    </div>
+    <NuxtLink
+      to="https://beian.miit.gov.cn/" target="_blank"
+    >
+      粤 ICP 备 20026972 号 - 1
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+a {
+  @apply text-base hover:text-momo no-underline hover:underline transition-all
+}
+</style>
