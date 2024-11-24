30e0a50c9e724a0cf9e2a58c0121c4f0e6306771
GeniusCorn <hi@nicecorn.com>
2024-11-24 18:15:47 +0800

    feat: add header

--- components/Header.vue
+++ components/Header.vue
@@ -4,17 +4,50 @@
 
 <template>
   <header>
-    <div class="flex flex-row gap-4">
-      <NuxtLink to="/">
-        Home
+    <div class="flex flex-row gap-4 items-center justify-between px-4 py-2">
+      <NuxtLink font-logo to="/">
+        CornPark
       </NuxtLink>
-      <NuxtLink to="/posts">
-        Posts
-      </NuxtLink>
+
+      <div class="flex flex-row gap-4 items-center">
+        <NuxtLink to="/">
+          <div i-ri-home-2-line />
+        </NuxtLink>
+
+        <NuxtLink to="/posts">
+          <div i-ri-article-line />
+        </NuxtLink>
+
+        <NuxtLink to="/projects">
+          <div i-ri-apps-line />
+        </NuxtLink>
+
+        <NuxtLink
+          to="https://github.com/GeniusCorn"
+          external
+          target="_blank"
+        >
+          <div i-ri-github-line />
+        </NuxtLink>
+
+        <NuxtLink
+          to="https://bsky.app/profile/nicecorn.bsky.social"
+          external
+          target="_blank"
+        >
+          <div i-ri-bluesky-line />
+        </NuxtLink>
+
+        <NuxtLink>
+          <div i-ri-moon-line />
+        </NuxtLink>
+      </div>
     </div>
   </header>
 </template>
 
 <style scoped>
-
+a {
+  @apply text-2xl text-black hover:text-momo transition-all no-underline;
+}
 </style>
--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,34 @@
-import { defineConfig } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTagify,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+} from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [
+    presetAttributify(),
+    presetUno(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      provider: 'bunny',
+      fonts: {
+        logo: 'ABeeZee',
+      },
+    }),
+    presetTagify(),
+  ],
+  transformers: [
+    transformerDirectives(),
+  ],
+  theme: {
+    colors: {
+      momo: '#f596aa',
+    },
+  },
 })
