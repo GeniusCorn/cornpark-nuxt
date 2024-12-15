35462decc264fbf89a1a50d0b21b4b592d777d68
GeniusCorn <hi@nicecorn.com>
2024-12-15 19:45:31 +0800

    feat: add ProseA

---
+++ components/content/ProseA.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <NuxtLink
+    target="_blank"
+    external underline decoration-slate-500 underline-offset-4 transition-all hover:text-momo text-base hover:decoration-momo
+  >
+    <slot />
+  </NuxtLink>
+</template>
